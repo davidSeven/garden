dfcbbd7534042f3f70c7153696e1b7e376bfc160
xxxcxy <yy_z3em@163.com>
2019-10-24 17:42:46 +0800

    add

---
+++ garden-city/sqlscripts/20191024/garden-i18n-20191024.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `sys_i18n_t` (
+  `ID` varchar(32) NOT NULL COMMENT 'ID',
+  `KEY` varchar(128) DEFAULT '' COMMENT '国际化KEY',
+  `VALUE` varchar(255) DEFAULT '' COMMENT '国际化值',
+  `LANGUAGE_TYPE` varchar(32) DEFAULT '' COMMENT '语言类型',
+  `STATE` varchar(2) DEFAULT '' COMMENT '状态',
+  `REMARK` varchar(255) DEFAULT '' COMMENT '备注',
+  `CREATED_BY` varchar(32) DEFAULT '' COMMENT '创建人',
+  `CREATION_DATE` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `UPDATED_BY` varchar(32) DEFAULT '' COMMENT '修改人',
+  `UPDATION_DATE` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间',
+  `ENABLED_FLAG` int(1) DEFAULT '1' COMMENT '是否禁用',
+  `TRACE_ID` varchar(16) DEFAULT '' COMMENT '日志ID',
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='国际化表';
+
