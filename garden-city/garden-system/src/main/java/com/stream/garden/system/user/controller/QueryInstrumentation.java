b335a1279002dff6c4aa8464225c2526a42ac636
xxxcxy <yy_z3em@163.com>
2020-05-25 20:04:36 +0800

    add

---
+++ garden-city/garden-system/src/main/java/com/stream/garden/system/user/controller/QueryInstrumentation.java
@@ -0,0 +1,28 @@
+package com.stream.garden.system.user.controller;
+
+import graphql.ExecutionInput;
+import graphql.ExecutionResult;
+import graphql.execution.instrumentation.*;
+import graphql.execution.instrumentation.parameters.*;
+import graphql.language.Document;
+import graphql.validation.ValidationError;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * @author zhangyuyuan
+ * @date 2020-05-25 16:18
+ */
+@Component
+public class QueryInstrumentation extends SimpleInstrumentation {
+    private Logger logger = LoggerFactory.getLogger(QueryInstrumentation.class);
+
+    @Override
+    public ExecutionInput instrumentExecutionInput(ExecutionInput executionInput, InstrumentationExecutionParameters parameters) {
+        logger.error("执行方法");
+        return executionInput;
+    }
+}
