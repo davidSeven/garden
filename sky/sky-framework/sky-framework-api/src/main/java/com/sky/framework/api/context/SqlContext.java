e955080075d0a22089b94881d7c7a36094bb67f9
xxxcxy <yy_z3em@163.com>
2020-11-21 21:11:57 +0800

    add

--- sky/sky-framework/sky-framework-api/src/main/java/com/sky/framework/api/context/SqlContext.java
+++ sky/sky-framework/sky-framework-api/src/main/java/com/sky/framework/api/context/SqlContext.java
@@ -5,6 +5,8 @@ import com.sky.framework.api.enums.DataScopeEnum;
 import java.util.List;
 
 /**
+ * 菜单配置 --> 配置接口URL --> 配置接口数据权限 --> 拦截器上获取到配置信息，将配置信息设置到上下文中 --> 在SQL拦截器中拼接SQL
+ *
  * @date 2020/11/21 20:47
  */
 public class SqlContext {
