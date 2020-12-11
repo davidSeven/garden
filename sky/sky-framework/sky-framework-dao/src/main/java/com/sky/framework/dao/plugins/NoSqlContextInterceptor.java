abca7b788fc857260f124de6ac164e2dab060c9c
xxxcxy <yy_z3em@163.com>
2020-12-11 22:21:32 +0800

    add

---
+++ sky/sky-framework/sky-framework-dao/src/main/java/com/sky/framework/dao/plugins/NoSqlContextInterceptor.java
@@ -0,0 +1,85 @@
+package com.sky.framework.dao.plugins;
+
+import org.apache.ibatis.cache.CacheKey;
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.executor.statement.StatementHandler;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.mapping.SqlCommandType;
+import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Method;
+import java.sql.Connection;
+import java.util.Properties;
+
+@Intercepts(
+        {
+                @Signature(type = StatementHandler.class, method = "prepare", args = {Connection.class, Integer.class}),
+                @Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class}),
+                @Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class, CacheKey.class, BoundSql.class}),
+        }
+)
+public class NoSqlContextInterceptor implements Interceptor {
+
+    private final Logger logger = LoggerFactory.getLogger(SqlContextInterceptor.class);
+
+    private Properties properties;
+
+    @Override
+    public Object intercept(Invocation invocation) throws Throwable {
+
+        Executor executor = (Executor) invocation.getTarget();
+
+        Method invocationMethod = invocation.getMethod();
+
+        Object[] invocationArgs = invocation.getArgs();
+
+        MappedStatement mappedStatement = (MappedStatement) invocationArgs[0];
+
+        SqlCommandType sqlCommandType = mappedStatement.getSqlCommandType();
+
+        if (!SqlCommandType.SELECT.equals(sqlCommandType)) {
+            return invocation.proceed();
+        }
+
+        Object parameter = invocationArgs[1];
+        RowBounds rowBounds = (RowBounds) invocationArgs[2];
+        ResultHandler resultHandler = (ResultHandler) invocationArgs[3];
+
+        CacheKey cacheKey;
+        BoundSql boundSql;
+
+        //由于逻辑关系，只会进入一次
+        if (invocationArgs.length == 4) {
+            //4 个参数时
+            boundSql = mappedStatement.getBoundSql(parameter);
+            cacheKey = executor.createCacheKey(mappedStatement, parameter, rowBounds, boundSql);
+        } else {
+            //6 个参数时
+            cacheKey = (CacheKey) invocationArgs[4];
+            boundSql = (BoundSql) invocationArgs[5];
+        }
+
+        // 在这里增加数据权限逻辑
+
+        return executor.query(mappedStatement, parameter, rowBounds, resultHandler, cacheKey, boundSql);
+    }
+
+    @Override
+    public Object plugin(Object target) {
+        if (target instanceof Executor) {
+            return Plugin.wrap(target, this);
+        }
+        return target;
+    }
+
+    @Override
+    public void setProperties(Properties properties) {
+        this.properties = properties;
+        this.logger.info(this.properties.toString());
+    }
+}
--- sky/sky-framework/sky-framework-utils/src/test/java/com/sky/framework/words/TestSensitiveWordsSearch.java
+++ sky/sky-framework/sky-framework-utils/src/test/java/com/sky/framework/words/TestSensitiveWordsSearch.java
@@ -17,19 +17,21 @@ public class TestSensitiveWordsSearch {
         SensitiveWordsSearch instance = SensitiveWordsSearch.getInstance();
 
         List<SensitiveWords> sensitiveWordsList = new ArrayList<>();
-        sensitiveWordsList.add(new SensitiveWords(1L, "中国", "华夏"));
-        sensitiveWordsList.add(new SensitiveWords(2L, "中国人", "华夏人"));
-        sensitiveWordsList.add(new SensitiveWords(3L, "中国人民", "华夏人民"));
-        sensitiveWordsList.add(new SensitiveWords(4L, "中共", "共党"));
-        sensitiveWordsList.add(new SensitiveWords(5L, "中共中央", "中国共党"));
-        sensitiveWordsList.add(new SensitiveWords(6L, "共产", "供铲"));
-        sensitiveWordsList.add(new SensitiveWords(7L, "中华", "华夏"));
+        sensitiveWordsList.add(new SensitiveWords(0L, "凌晨", "半夜"));
+        sensitiveWordsList.add(new SensitiveWords(1L, "凌晨两点", "丑时三刻"));
+        sensitiveWordsList.add(new SensitiveWords(2L, "国庆", "庆国"));
+        sensitiveWordsList.add(new SensitiveWords(3L, "阅兵", "大阅"));
+        sensitiveWordsList.add(new SensitiveWords(4L, "七点", "辰时"));
+        sensitiveWordsList.add(new SensitiveWords(5L, "战地", "战场"));
+        sensitiveWordsList.add(new SensitiveWords(6L, "维和军士", "和平使者"));
+        sensitiveWordsList.add(new SensitiveWords(7L, "特警", "使者"));
+        sensitiveWordsList.add(new SensitiveWords(8L, "小说", "软文"));
         instance.updateKeywords(sensitiveWordsList);
 
         System.out.println("已加载关键词：");
         System.out.println(JSON.toJSONString(sensitiveWordsList));
 
-        String text = "中国共产党是中国工人阶级的先锋队，同时是中国人民和中华民族的先锋队，是中国特色社会主义事业的领导核心，代表中国先进生产力的发展要求，代表中国先进文化的前进方向，代表中国最广大人民的根本利益。党的最高理想和最终目标是实现共产主义";
+        String text = "凌晨凌晨两点毫无睡意，受国庆阅兵影响，七点爬起来看《白色橄榄树》这部小说。无论怎样评价玖月晞，战地记者与维和军士的配置简直招架不住。阿瓒又成为所看过小说里难以忘怀的一个名字，他是柔和淡定的排弹士兵，是机场突然出现的特警，也是在东国苏睿城郊5秒救下她的人。玖月晞轻描淡写，出来的故事却刻骨铭心，真的很喜欢这样力度的小说。";
         System.out.println("查询文本：\n" + text);
 
         SensitiveWordsReplace.init(sensitiveWordsList);
