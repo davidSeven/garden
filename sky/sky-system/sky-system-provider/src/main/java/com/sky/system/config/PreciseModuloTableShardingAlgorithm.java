80eef97ea74142d832a57dc00eaeed31a3f57011
xxxcxy <yy_z3em@163.com>
2021-07-09 16:11:02 +0800

    add

--- sky/sky-system/sky-system-provider/src/main/java/com/sky/system/config/PreciseModuloTableShardingAlgorithm.java
+++ sky/sky-system/sky-system-provider/src/main/java/com/sky/system/config/PreciseModuloTableShardingAlgorithm.java
@@ -1,6 +1,5 @@
 package com.sky.system.config;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.shardingsphere.api.sharding.standard.PreciseShardingAlgorithm;
 import org.apache.shardingsphere.api.sharding.standard.PreciseShardingValue;
 
@@ -10,26 +9,24 @@ import java.util.Collection;
  * @author zhangyuyuan
  * @date 2021-07-01 21:20
  */
-public class PreciseModuloTableShardingAlgorithm implements PreciseShardingAlgorithm<String> {
+public class PreciseModuloTableShardingAlgorithm implements PreciseShardingAlgorithm<Long> {
 
     @Override
-    public String doSharding(Collection<String> collection, PreciseShardingValue<String> preciseShardingValue) {
-        //对于库的分片collection存放的是所有的库的列表，这里代表flow_01~flow_12
-        //配置的分片的sharding-column对应的值
-        String timeValue = preciseShardingValue.getValue();
-        //分库时配置的sharding-column
-        String time = preciseShardingValue.getColumnName();
-        //需要分库的逻辑表
-        String table = preciseShardingValue.getLogicTableName();
-        if (StringUtils.isBlank(timeValue)) {
-            throw new UnsupportedOperationException("preciseShardingValue is null");
+    public String doSharding(Collection<String> collection, PreciseShardingValue<Long> preciseShardingValue) {
+        // 对于库的分片collection存放的是所有的库的列表，这里代表flow_01~flow_12
+        // 配置的分片的sharding-column对应的值
+        Long idValue = preciseShardingValue.getValue();
+        // 分库时配置的sharding-column
+        // String idColumn = preciseShardingValue.getColumnName();
+        String logicTableName = preciseShardingValue.getLogicTableName();
+        // 需要分库的逻辑表
+        int tableIndex = 0;
+        if (idValue > 10) {
+            tableIndex = 1;
         }
-        //按月路由
-        for (String each : collection) {
-            String value = StringUtils.substring(timeValue, 4, 6); //获取到月份
-            if (each.endsWith(value)) {
-                //这里返回回去的就是最终需要查询的表名
-                return each;
+        for (String tableName : collection) {
+            if (tableName.equals(logicTableName + tableIndex)) {
+                return tableName;
             }
         }
         return null;
