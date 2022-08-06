020df7f855d8df8306f3dd536f2b8668d48860f4
xxxcxy <yy_z3em@163.com>
2022-08-06 16:05:22 +0800

    add

---
+++ example/example-mybatis/src/main/java/com/example/model/Test.java
@@ -0,0 +1,53 @@
+package com.example.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Test implements Serializable {
+
+    private Long id;
+    private String code;
+    private String name;
+    private Date createTime;
+    private String remark;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+}
